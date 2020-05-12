e22d356f37efb84014876133b0b0f9379a99af3c
immutable-integer-8885 <gidelbusto@gmail.com>
2020-05-12 13:56:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "NO, NOT SINCE 1938!"
-  elsif phrase.upcase
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
